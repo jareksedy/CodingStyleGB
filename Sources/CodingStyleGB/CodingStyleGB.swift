e25504ff56438f1dbb01e91a5d4f4d88a12378b9
Jarek <jareksedy@icloud.com>
2021-11-06 17:51:01 +0600

    Added tests.

--- Sources/CodingStyleGB/CodingStyleGB.swift
+++ Sources/CodingStyleGB/CodingStyleGB.swift
@@ -1,6 +1,67 @@
+@propertyWrapper
 public struct CodingStyleGB {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
+    
+    public enum Style: String {
+        case CamelCase  = " "
+        case SnakeCase  = "_"
+        case KebabCase  = "-"
+    }
+    
+    private var value: String
+    private var style: Style
+    
+    public var wrappedValue: String {
+        get { сoncatenation(array: getSubSequence()) }
+        set { value = newValue }
+    }
+    
+    public var projectedValue: Style {
+        get { style }
+        set { style = newValue }
+    }
+    
+    
+    public init(wrappedValue: String, style: Style) {
+        self.value = wrappedValue
+        self.style = style
+    }
+    
+    //  MARK:   - Сonverting string to a given style
+    
+    private func placesSpacesBeforeCapitalLetters() -> String {
+        var newString: String = ""
+        
+        value.forEach { char in
+            if char.isUppercase { newString.append(" ") }
+            newString.append(char)
+        }
+        
+        if newString.first == " " { newString.removeFirst() }
+        
+        return newString
+    }
+    
+    private func getSubSequence() -> [String] {
+        let substrings: [String] = placesSpacesBeforeCapitalLetters().split { separator in
+            if separator == " " || separator == "-" || separator == "_" { return true }
+            return false
+        }.map { String($0).lowercased() }
+        
+        return substrings
+    }
+    
+    private func сoncatenation(array: [String]) -> String {
+        var output: String = ""
+        
+        switch style {
+        case .CamelCase:
+            array.forEach { output += $0.prefix(1).uppercased() + $0.dropFirst() }
+            
+        default:
+            array.forEach { output += $0 + style.rawValue }
+            if !output.isEmpty { output.removeLast() }
+        }
+        
+        return output
     }
 }
--- Tests/CodingStyleGBTests/CodingStyleGBTests.swift
+++ Tests/CodingStyleGBTests/CodingStyleGBTests.swift
@@ -1,11 +1,16 @@
 import XCTest
+import SwiftUI
 @testable import CodingStyleGB
 
 final class CodingStyleGBTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(CodingStyleGB().text, "Hello, World!")
+    
+    @CodingStyleGB(style: .SnakeCase) var testSnake = "test snake case"
+    @CodingStyleGB(style: .CamelCase) var testCamel = "Camel test"
+    @CodingStyleGB(style: .KebabCase) var testKebab = "How about some kebab?"
+    
+    func testCodingStyleGB() throws {
+        XCTAssertEqual(testSnake, "test_snake_case")
+        XCTAssertEqual(testCamel, "CamelTest")
+        XCTAssertEqual(testKebab, "how-about-some-kebab?")
     }
 }
